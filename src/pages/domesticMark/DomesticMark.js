be64e3ffde0cf309a6f61e1651bab38d833c8b26
ljw43 <leejiwon_tech@naver.com>
2023-04-20 17:47:35 +0900

    Restructure data object, handle axios errors, and add applicantType useState
    data 객체의 구조를 mark, applicant, user 객체를 포함하도록 변경.
    axios 오류 처리 추가.
    applicantType을 useState로 추가하고 ApplicantForm 컴포넌트의 onAppliantChange props에 setApplicantType 함수 전달.

--- src/pages/domesticMark/DomesticMark.js
+++ src/pages/domesticMark/DomesticMark.js
@@ -16,18 +16,70 @@ function DomesticMark() {
   const [classificationData, setClassificationData] = useState({});
   const [managerData, setManagerData] = useState({});
   const [applicantData, setApplicantData] = useState({});
+  const [applicantType, setApplicantType] = useState({ poc: "personal" });
 
+  const markSelectData = {
+    type: "국내",
+  };
+
+  const nationData = {
+    country: "대한민국",
+    madrid: "마드리드",
+    direct: "각국출원",
+    status: "진행상태",
+  };
   const handleSubmit = () => {
     const data = {
-      //...markSelectData
-      ...trademarkData,
-      ...classificationData,
-      ...managerData,
-      //...nationData,
-      ...applicantData,
+      mark: {
+        ...trademarkData,
+        ...classificationData,
+        ...markSelectData,
+        ...applicantType,
+        ...nationData,
+      },
+      ...(applicantType.poc === "personal"
+        ? { personal: { ...applicantData } }
+        : { corporate: { ...applicantData } }),
+      user: { ...managerData },
     };
+
+    // 테스트용 데이터
+    const data2 = {
+      mark: {
+        brand_name: "123",
+        description: "123",
+        image: "123",
+        sector: "123",
+        type: "123",
+        poc: "personal",
+        country: "123",
+        madrid: "123",
+        direct: "123",
+        status: "123",
+      },
+      personal: {
+        name_kor: "456",
+        name_eng: "456",
+        ssn: "456",
+        personalEmail: "aaa@aaa.com",
+        personalMobile: "456",
+        personalPhone: "456",
+        address: "456",
+        detail: "456",
+        zipcode: "456",
+        agreement: "456",
+      },
+      user: {
+        name: "999",
+        email: "aaa@bb.com",
+        mobile: "999",
+        phone: "999",
+        acc_num: "999",
+      },
+    };
+    console.log(data);
     const endpoint =
-      applicantData.type === "personal"
+      applicantType.poc === "personal"
         ? "http://localhost:8080/api/register/personal"
         : "http://localhost:8080/api/register/corporate";
 
@@ -41,8 +93,11 @@ function DomesticMark() {
         },
       })
       .then((response) => {
-        console.log("리스폰스");
         console.log(response);
+      })
+      .catch((error) => {
+        console.log("에러");
+        console.log(error);
       });
   };
 
@@ -54,7 +109,10 @@ function DomesticMark() {
       <ClassificationForm onClassificationataChange={setClassificationData} />
       <ManagerForm onManagerChange={setManagerData} />
       <NationSelectForm />
-      <ApplicantForm onApplicantChange={setApplicantData} />
+      <ApplicantForm
+        onApplicantChange={setApplicantData}
+        onApplicantTypeChange={setApplicantType}
+      />
       <Button onClick={handleSubmit} variant="contained">
         신청하기
       </Button>
