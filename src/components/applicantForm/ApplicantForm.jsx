b83abb864f3c87657a98bf9ef5ab601004c583f9
ljw43 <leejiwon_tech@naver.com>
2023-04-20 18:01:13 +0900

    ApplicantForm 업데이트: applicantType 변경 및 onApplicantChange props 추가
    - applicantType의 초기값을 {poc: "personal"}로 변경
    - 변경된 applicantType 객체의 구조에 따라 applicantType을 applicantType.poc으로 변경
    - applicantType 상태가 변경될 때마다 onApplicantTypeChange 함수를 호출하는 useEffect 훅 추가
    - ApplicantForm props에 onApplicnatChange 함수 추가

--- src/components/applicantForm/ApplicantForm.jsx
+++ src/components/applicantForm/ApplicantForm.jsx
@@ -12,26 +12,34 @@ import { useState, useEffect } from "react";
 import PersonalForm from "./personalForm/PersonalForm";
 import CorporateForm from "./corporateForm/CorporateForm";
 
-const ApplicantForm = ({ onApplicantChange }) => {
-  const [applicantType, setApplicantType] = useState("personal");
+const ApplicantForm = ({ onApplicantChange, onApplicantTypeChange }) => {
+  const [applicantType, setApplicantType] = useState({ poc: "personal" });
   const [corporateData, setCorporateData] = useState({});
   const [personalData, setPersonalData] = useState({});
   const [applicantData, setApplicantData] = useState({});
 
   const handleChange = (event) => {
-    setApplicantType(event.target.value);
+    const value = event.target.value;
+    setApplicantType(() => ({
+      poc: value,
+    }));
   };
 
   useEffect(() => {
-    onApplicantChange(applicantData);
-  }, [applicantData, onApplicantChange]);
+    onApplicantTypeChange(applicantType);
+  }, [applicantType, onApplicantTypeChange]);
 
   useEffect(() => {
     // applicantType에 따라 personalData 또는 corporateData를 applicantData에 저장
     const dataToSave =
-      applicantType === "personal" ? personalData : corporateData;
-    setApplicantData({ ...dataToSave, type: applicantType });
+      applicantType.poc === "personal" ? personalData : corporateData;
+    setApplicantData({ ...dataToSave });
+    console.log(dataToSave);
   }, [applicantType, personalData, corporateData]);
+
+  useEffect(() => {
+    onApplicantChange(applicantData);
+  }, [applicantData, onApplicantChange]);
 
   return (
     <FormContainer>
@@ -40,7 +48,7 @@ const ApplicantForm = ({ onApplicantChange }) => {
           <Label>출원인 유형을 선택해주세요</Label>
           <RadioGroup
             name="applicant-type"
-            value={applicantType}
+            value={applicantType.poc}
             onChange={handleChange}
             sx={{
               display: "flex",
@@ -80,7 +88,7 @@ const ApplicantForm = ({ onApplicantChange }) => {
             05. 출원인 정보를 입력해주세요
           </CustomTypo>
         </ApplicantWrapper>
-        {applicantType === "personal" ? (
+        {applicantType.poc === "personal" ? (
           <PersonalForm onPersonalChange={setPersonalData} />
         ) : (
           <CorporateForm onCorporateChange={setCorporateData} />
