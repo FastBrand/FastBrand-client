00e6ee09c1518df2ee33de0b359b53dba2d2f0a0
kyw <96057775+blueberrycoffee@users.noreply.github.com>
2023-04-21 18:02:10 +0900

    Revert "."
    This reverts commit e0c1d6291b1123da7ba2de31a14f59dd1d23dab6.

--- craco.config.js
+++
@@ -1,9 +0,0 @@
-// craco.config.js 파일
-module.exports = {
-  webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.devServer.allowedHosts = ['localhost'];
-      return webpackConfig;
-    }
-  }
-}
