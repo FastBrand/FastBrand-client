92add5694dce0b8c3d68d7f027ff641edce03f1e
mwjng <wmw04087@gmail.com>
2023-05-10 03:54:07 +0900

    페이지 표시

--- src/admin/compoents/MarkInfo/MarkInfo.jsx
+++ src/admin/compoents/MarkInfo/MarkInfo.jsx
@@ -5,12 +5,24 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
 import axios from "axios";
+import ReactPaginate from "react-paginate";
+import "./paginate.css"
 
 
 function MarkInfo() {
     const [trademarks, setTrademarks] = useState([]);
     const location = useLocation();
-  
+
+    const [pageNumber, setPageNumber] = useState(0);
+    const itemsPerPage = 10;
+    const pagesVisited = pageNumber * itemsPerPage;
+
+    const pageCount = Math.ceil(trademarks.length / itemsPerPage);
+
+    const changePage = ({ selected }) => {
+        setPageNumber(selected);
+    };
+
     const refreshData = () => {
       axios.get("http://localhost:8080/api/main/info")
         .then(response => {
@@ -44,30 +56,43 @@ function MarkInfo() {
         <TableContainer component={Paper} sx={{ width: "80%" }}>
         <Table>
           <TableHead>
-            <TableRow>
-              <TableCell align="center">상표번호</TableCell>
-              <TableCell>상표명</TableCell>
-              <TableCell>출원유형</TableCell>
-              <TableCell>개인/법인</TableCell>
-              <TableCell>담당자</TableCell>
-              <TableCell>이메일</TableCell>
+            <TableRow sx={{ background: "#9E9E9F" }}>
+              <TableCell align="center"><b>상표번호</b></TableCell>
+              <TableCell align="center"><b>상표명</b></TableCell>
+              <TableCell align="center"><b>출원유형</b></TableCell>
+              <TableCell align="center"><b>개인/법인</b></TableCell>
+              <TableCell align="center"><b>담당자</b></TableCell>
+              <TableCell align="center"><b>이메일</b></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {trademarks.map((trademark) => (
+            {trademarks.slice(pagesVisited, pagesVisited + itemsPerPage).map((trademark) => (
               <TableRow key={trademark.id}>
-                <TableCell>{trademark.id}</TableCell>
-                <TableCell>{trademark.name}</TableCell>
-                <TableCell>{trademark.type}</TableCell>
-                <TableCell>{trademark.poc}</TableCell>
-                <TableCell>{trademark.name}</TableCell>
-                <TableCell>{trademark.email}</TableCell>
+                <TableCell align="center">{trademark.id}</TableCell>
+                <TableCell align="center">{trademark.name}</TableCell>
+                <TableCell align="center">{trademark.type}</TableCell>
+                <TableCell align="center">{trademark.poc}</TableCell>
+                <TableCell align="center">{trademark.name}</TableCell>
+                <TableCell align="center">{trademark.email}</TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
         </TableContainer>
+        <div>
+            <ReactPaginate
+                previousLabel={'이전'}
+                nextLabel={'다음'}
+                pageCount={pageCount}
+                onPageChange={changePage}
+                containerClassName={'pagination'}
+                previousLinkClassName={'pagination__link'}
+                nextLinkClassName={'pagination__link'}
+                disabledClassName={'pagination__link--disabled'}
+                activeClassName={'pagination__link--active'}
+            />
         </div>
+    </div>
     );
 }
 
