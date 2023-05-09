3e64755f51a53a204aeb6c2252d3b96342923057
mwjng <wmw04087@gmail.com>
2023-05-10 02:36:09 +0900

    Update MarkInfo.jsx

--- src/admin/compoents/MarkInfo/MarkInfo.jsx
+++ src/admin/compoents/MarkInfo/MarkInfo.jsx
@@ -1,27 +1,31 @@
 import { Paper,Table,TableCell,TableContainer,TableHead,TableRow,TableBody,Box,Button,IconButton, } from '@mui/material';
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
 import axios from "axios";
 
+
 function MarkInfo() {
     const [trademarks, setTrademarks] = useState([]);
+    const location = useLocation();
   
     const refreshData = () => {
       axios.get("http://localhost:8080/api/main/info")
         .then(response => {
           const dataArr = response.data;
           const newTrademarks = dataArr.map(data => {
-            const { brand_name, poc, type } = data.mark;
+            const { id, brand_name, poc, type } = data.mark;
             const { name, email } = data.user;
   
             return {
+              id: id,  
               name: brand_name,
               poc: poc,
               type: type,
               name: name,
-              email: email
+              email: email,
             };
           });
   
@@ -41,7 +45,8 @@ function MarkInfo() {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell align="center">상표명</TableCell>
+              <TableCell align="center">상표번호</TableCell>
+              <TableCell>상표명</TableCell>
               <TableCell>출원유형</TableCell>
               <TableCell>개인/법인</TableCell>
               <TableCell>담당자</TableCell>
@@ -49,8 +54,9 @@ function MarkInfo() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {trademarks.map((trademark, index) => (
-              <TableRow key={index}>
+            {trademarks.map((trademark) => (
+              <TableRow key={trademark.id}>
+                <TableCell>{trademark.id}</TableCell>
                 <TableCell>{trademark.name}</TableCell>
                 <TableCell>{trademark.type}</TableCell>
                 <TableCell>{trademark.poc}</TableCell>
@@ -61,7 +67,7 @@ function MarkInfo() {
           </TableBody>
         </Table>
         </TableContainer>
-      </div>
+        </div>
     );
 }
 
