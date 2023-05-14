96c3f634510b47e5cc944565d9a75085c0393bdf
ljw43 <leejiwon_tech@naver.com>
2023-05-14 18:24:24 +0900

    Add Footer and change MainStyle

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,38 +1,39 @@
-import { CustomBox, Title, BackgroundBox, ButtonLink } from "./MainStyle";
-import { Box, Typography, Container } from "@mui/material";
+import {
+  CustomBox,
+  Title,
+  BackgroundBox,
+  ButtonLink,
+  MainContainer,
+  SubTitle,
+} from "./MainStyle";
+import { Box, Typography } from "@mui/material";
+import Footer from "../../components/footer/Footer";
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 
 const Main = () => {
   return (
-    <BackgroundBox>
-      <Container>
-        <Navbar backgroundColor={false} borderBottom={false} />
-        <CustomBox>
-          <Box>
-            <Title variant="h1">
-              신속하고 합리적인
-              <br />
-              <Box component="span" sx={{ color: "#CBA585" }}>
-                상표등록
-              </Box>
-            </Title>
-            <Typography
-              sx={{
-                fontFamily: "Pretendard",
-                fontSize: "20px",
-                color: "white",
-                fontWeight: "500",
-                margin: "0 0 40px 5px",
-              }}
-            >
-              고민하지 말고 신청하세요!
-            </Typography>
-            <ButtonLink to="/domesticMark">상표등록 신청하기</ButtonLink>
-          </Box>
-        </CustomBox>
-      </Container>
-    </BackgroundBox>
+    <>
+      <BackgroundBox>
+        <MainContainer>
+          <Navbar backgroundColor={false} borderBottom={false} />
+          <CustomBox>
+            <Box>
+              <Title variant="h1">
+                신속하고 합리적인
+                <br />
+                <Box component="span" sx={{ color: "#CBA585" }}>
+                  상표등록
+                </Box>
+              </Title>
+              <SubTitle sx={{}}>고민하지 말고 신청하세요!</SubTitle>
+              <ButtonLink to="/domesticMark">상표등록 신청하기</ButtonLink>
+            </Box>
+          </CustomBox>
+        </MainContainer>
+      </BackgroundBox>
+      <Footer />
+    </>
   );
 };
 
