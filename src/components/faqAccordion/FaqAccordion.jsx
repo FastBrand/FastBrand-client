c606bb984195b66252e5144c01dc974b0dbf9b36
ljw43 <leejiwon_tech@naver.com>
2023-05-17 23:32:40 +0900

    줄바꿈 처리

--- src/components/faqAccordion/FaqAccordion.jsx
+++ src/components/faqAccordion/FaqAccordion.jsx
@@ -12,7 +12,12 @@ const FaqAccordion = () => {
     axios
       .get("http://localhost:8080/api/faq")
       .then((response) => {
-        setFaqData(response.data);
+        const formattedData = response.data.map((faq) => ({
+          ...faq,
+          content: faq.content.replace(/\\r\\n/g, "\r\n"),
+          title: faq.title.replace(/\\r\\n/g, "\r\n"),
+        }));
+        setFaqData(formattedData);
       })
       .catch((error) => {
         console.log(error);
@@ -28,10 +33,13 @@ const FaqAccordion = () => {
             aria-controls="panel1a-content"
             id="panel1a-header"
           >
-            <Title>Q. {faq.title}</Title>
+            <Title>
+              <span style={{ color: "#CBA585" }}>Q. </span>
+              {faq.title}
+            </Title>
           </AccordionSummary>
           <AccordionDetails>
-            <Content>{faq.content}</Content>
+            <Content style={{ whiteSpace: "pre-line" }}>{faq.content}</Content>
           </AccordionDetails>
         </Accordion>
       ))}
