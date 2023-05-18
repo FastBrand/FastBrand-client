89b8f11572fbd94235f546fb3c5b9d003eba464a
ljw43 <leejiwon_tech@naver.com>
2023-05-18 20:34:07 +0900

    Update CheckModal.js
    - 모달창 박스 border 삭제
    - 모달창 borderRadius:3px 추가
    - checkText01 폰트 굵게

--- src/components/CheckModal/CheckModal.js
+++ src/components/CheckModal/CheckModal.js
@@ -1,100 +1,98 @@
 import {
-  Modal, 
+  Modal,
   Box,
-  IconButton, 
+  IconButton,
   Button,
-  Table, 
+  Table,
   TableHead,
-  TableBody, 
-  TableRow, 
-  TableCell 
- } from "@mui/material";
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import { makeStyles } from '@material-ui/styles';
-import emailjs from 'emailjs-com';
+import { makeStyles } from "@material-ui/styles";
+import emailjs from "emailjs-com";
 
-const useStyles = makeStyles((theme)=>({
-  modalBox:{
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    border: '1px solid #000',
+const useStyles = makeStyles((theme) => ({
+  modalBox: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    // border: '1px solid #000',
+    borderRadius: "3px",
     boxShadow: 18,
     p: 8,
-    overflow: 'auto',
-    width: '600px',
-    height: '700px',
-    backgroundColor:'white',
+    overflow: "auto",
+    width: "600px",
+    height: "700px",
+    backgroundColor: "white",
   },
-  modalClose:{
-    position: 'absolute',
-    top: '5px',
-    right: '5px',
+  modalClose: {
+    position: "absolute",
+    top: "5px",
+    right: "5px",
   },
-  checkText01:{
+  checkText01: {
     fontFamily: "Pretendard",
-    fontSize: '22px',
-    fontWeight: 300,
-    color: 'black',
-    textAlign: 'left',
-    marginLeft: '10px',
-    marginTop: '25px',
-    marginBottom: '5px',
+    fontSize: "22px",
+    fontWeight: 600,
+    color: "black",
+    textAlign: "left",
+    marginLeft: "10px",
+    marginTop: "25px",
+    marginBottom: "5px",
   },
-  minicheckText01:{
+  minicheckText01: {
     fontFamily: "Pretendard",
-    textAlign: 'left',
-    fontSize: '14px',
-    marginLeft: '10px',
-    color: 'red',
+    textAlign: "left",
+    fontSize: "14px",
+    marginLeft: "10px",
+    color: "red",
     fontWeight: 400,
   },
-  checkTextBox:{
-    marginLeft: '50px',
-    marginRight: '50px',
-    marginTop: '50px',
-    borderBottom: '1px solid #cba585',
-    color: 'black',
-    textAlign: 'center',
+  checkTextBox: {
+    marginLeft: "50px",
+    marginRight: "50px",
+    marginTop: "50px",
+    borderBottom: "1px solid #cba585",
+    color: "black",
+    textAlign: "center",
   },
   tableCell: {
     fontFamily: "Pretendard",
-    borderRight: '0.1px solid black',
-    width: '50%',
-  }
-}))
-
+    borderRight: "0.1px solid black",
+    width: "50%",
+  },
+}));
 
 function CheckModal({
-  open,handleClose,handleSubmit, 
+  open,
+  handleClose,
+  handleSubmit,
   trademarkData,
-  madridDataString, 
-  directNationString, 
-  managerData, 
-  applicantData, 
-  markSelectData, 
+  madridDataString,
+  directNationString,
+  managerData,
+  applicantData,
+  markSelectData,
   classificationData,
   applicantType,
   madridPriceData,
-  directPriceData
+  directPriceData,
 }) {
-
   const classes = useStyles();
 
   const handlePrice = () => {
     let priceData = 0;
-   
-    if (markSelectData === "국내출원"){
+
+    if (markSelectData === "국내출원") {
       priceData = madridPriceData + directPriceData + 200000;
       priceData = priceData.toFixed(0);
-     
-    }
-    else if (markSelectData === "국내+해외출원"){
+    } else if (markSelectData === "국내+해외출원") {
       priceData = madridPriceData + directPriceData + 100000;
       priceData = priceData.toFixed(0);
-    }
-    else {
+    } else {
       priceData = madridPriceData + directPriceData;
       priceData = priceData.toFixed(0);
     }
@@ -103,20 +101,19 @@ function CheckModal({
       currency: "KRW",
     });
     const numberWithCommas = formatter.format(priceData);
-    
-    return numberWithCommas
+
+    return numberWithCommas;
   };
-      // handlePrice 함수를 호출하여 가격을 계산
+  // handlePrice 함수를 호출하여 가격을 계산
   const formattedPrice = handlePrice();
-  
 
   const handleConfirmButtonClick = () => {
     if (window.confirm("정말로 진행하시겠습니까?")) {
-    handleSubmit();
-    sendEmail(); 
+      handleSubmit();
+      sendEmail();
     }
   };
-  
+
   const sendEmail = () => {
     let message;
     if (applicantType.poc === "personal") {
@@ -174,87 +171,100 @@ function CheckModal({
         법인대표 유선전화: ${applicantData.corporatePhone}
       `;
     }
-  
+
     const templateParams = {
-      subject: '상표신청',
-      message: message
+      subject: "상표신청",
+      message: message,
     };
-  
-    emailjs.send('service_ntfee7r', 'template_5zsy56b', templateParams, 'niIZOtG66JjWR0wjS')
-    .then((response) => {
-      console.log('이메일 전송성공', response);
-    })
-    .catch((error) => {
-      console.error('이메일 전송오류', error);
-    });
+
+    emailjs
+      .send(
+        "service_ntfee7r",
+        "template_5zsy56b",
+        templateParams,
+        "niIZOtG66JjWR0wjS"
+      )
+      .then((response) => {
+        console.log("이메일 전송성공", response);
+      })
+      .catch((error) => {
+        console.error("이메일 전송오류", error);
+      });
   };
 
   return (
     <Modal open={open} onClose={handleClose}>
       <Box className={classes.modalBox}>
-        <Box className={classes.modalClose} >
-        <IconButton onClick={handleClose}>
-          <CloseIcon />
-        </IconButton>
+        <Box className={classes.modalClose}>
+          <IconButton onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
         </Box>
         <div className={classes.checkText01}>견적 내용</div>
         <div className={classes.minicheckText01}>
-         ※ 최소가격은 1개류 기준으로 제시됩니다.<br/>
+          ※ 최소가격은 1개류 기준으로 제시됩니다.
+          <br />
           2개 이상의 분류를 선택했을 시, 실제 가격과 차이가 있을 수 있습니다.
-          </div>
+        </div>
         <Table>
-      <TableBody>
-        <TableRow>
-          <TableCell>패키지: </TableCell>
-          <TableCell>{markSelectData}</TableCell>
-        </TableRow>
-        <TableRow>
-        <TableCell>상표명: </TableCell>
-          <TableCell>{trademarkData.brand_name}</TableCell>
-        </TableRow>
-        <TableRow>
-        <TableCell>세부설명: </TableCell>
-          <TableCell>{trademarkData.description}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>예상최소가격: </TableCell>
-          <TableCell>{formattedPrice}원</TableCell>
-        </TableRow>
-        <TableRow>
-        <TableCell>상표 분류: </TableCell>
-        <TableCell>{classificationData.sector} </TableCell>
-        </TableRow>
-        <TableRow>
-        <TableCell>출원국가(마드리드): </TableCell>
-        <TableCell>{madridDataString}</TableCell>
-        </TableRow>
-        <TableRow>
-        <TableCell>출원국가(개별국출원):</TableCell>
-        <TableCell>{directNationString}</TableCell>
-        </TableRow>
+          <TableBody>
+            <TableRow>
+              <TableCell>패키지: </TableCell>
+              <TableCell>{markSelectData}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>상표명: </TableCell>
+              <TableCell>{trademarkData.brand_name}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>세부설명: </TableCell>
+              <TableCell>{trademarkData.description}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>예상최소가격: </TableCell>
+              <TableCell>{formattedPrice}원</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>상표 분류: </TableCell>
+              <TableCell>{classificationData.sector} </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>출원국가(마드리드): </TableCell>
+              <TableCell>{madridDataString}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>출원국가(개별국출원):</TableCell>
+              <TableCell>{directNationString}</TableCell>
+            </TableRow>
 
-        <TableRow>
-        <TableCell>출원인 성명: </TableCell>
-          <TableCell>{applicantData.name_kor}</TableCell>
-        </TableRow>
+            <TableRow>
+              <TableCell>출원인 성명: </TableCell>
+              <TableCell>{applicantData.name_kor}</TableCell>
+            </TableRow>
 
-        <TableRow>
-        <TableCell className={classes.tableCell}>담당자 성명: {managerData.name}</TableCell>
-        <TableCell>담당자 이메일: {managerData.email}</TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell className={classes.tableCell}>담당자 연락처: {managerData.mobile}</TableCell>
-        <TableCell>담당자 유선전화: {managerData.phone}</TableCell>
-      </TableRow>
-      </TableBody>
-    </Table>
-        <Button id="submitButton03"
-        onClick={handleConfirmButtonClick}
-        variant="contained">
-        견적발송
+            <TableRow>
+              <TableCell className={classes.tableCell}>
+                담당자 성명: {managerData.name}
+              </TableCell>
+              <TableCell>담당자 이메일: {managerData.email}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className={classes.tableCell}>
+                담당자 연락처: {managerData.mobile}
+              </TableCell>
+              <TableCell>담당자 유선전화: {managerData.phone}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        <Button
+          id="submitButton03"
+          onClick={handleConfirmButtonClick}
+          variant="contained"
+        >
+          견적발송
         </Button>
       </Box>
-  </Modal>
+    </Modal>
   );
-};
+}
 export default CheckModal;
