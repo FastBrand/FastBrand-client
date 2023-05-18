dfde769cfd34fec2e6c63f04c5c9dfb15b0f9d52
ljw43 <leejiwon_tech@naver.com>
2023-05-18 14:32:49 +0900

    이미지 파일 업로드 스타일 수정
    - theme.breakpoints.down("md")일 때 아이템 배치 수정

--- src/styles/FormStyles.jsx
+++ src/styles/FormStyles.jsx
@@ -1,6 +1,6 @@
 // 신청 폼 스타일
 import { styled } from "@mui/system";
-import { Button, Typography } from "@mui/material";
+import { Button, TextField, Typography } from "@mui/material";
 
 export const CustomTypo = styled(Typography)(({ theme }) => ({
   fontFamily: "Pretendard",
@@ -10,7 +10,7 @@ export const CustomTypo = styled(Typography)(({ theme }) => ({
 }));
 
 export const FormContainer = styled("div")(({ theme }) => ({
-  padding: "100px 13vw", //top right bottom left,
+  padding: "100px 13vw",
   [theme.breakpoints.down("xl")]: {
     paddingLeft: "5vw",
     paddingRight: "5vw",
@@ -37,6 +37,26 @@ export const FileLabel = styled("label")({
   fontSize: "25px",
 });
 
+export const FileUploadContainer = styled("div")(({ theme }) => ({
+  paddingLeft: "13vw",
+  paddingRight: "13vw",
+  backgroundColor: "#eee",
+  height: "180px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  [theme.breakpoints.down("xl")]: {
+    paddingLeft: "5vw",
+    paddingRight: "5vw",
+  },
+  [theme.breakpoints.down("md")]: {
+    flexDirection: "column",
+    paddingTop: "10px",
+    paddingBottom: "10px",
+    height: "150px",
+  },
+}));
+
 export const PostCodeButton = styled(Button)({
   borderRadius: "30px",
   backgroundColor: "#d9d9d9",
@@ -46,5 +66,26 @@ export const PostCodeButton = styled(Button)({
   "&:hover": {
     backgroundColor: "#d9d9d9",
     color: "black",
+  },
+});
+
+export const CustomTextField = styled(TextField)({
+  // 폰트 설정
+  fontFamily: "Pretendard",
+  fontWeight: "400",
+
+  "& label.Mui-focused": {
+    // focused 상태일 때의 라벨 색상
+    color: "#f44336",
+  },
+  "& .MuiInputBase-root": {
+    // 기본 상태일 때의 폰트와 밑줄 색상
+    fontFamily: "Pretendard",
+    fontWeight: "400",
+    borderBottom: "1px solid #757575",
+    "&.Mui-focused": {
+      // focused 상태일 때의 폰트와 밑줄 색상
+      borderBottom: "2px solid black",
+    },
   },
 });
