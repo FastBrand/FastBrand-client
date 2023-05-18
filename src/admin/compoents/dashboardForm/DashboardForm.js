a1c1988564b660c313c961bca7bb83fcb66358b0
ljw43 <leejiwon_tech@naver.com>
2023-05-18 20:31:37 +0900

    Update DashboardForm.js
    - 대시보드 text01 underline 삭제, 폰트굵기 두껍게 변경
    - box border 삭제

--- src/admin/compoents/dashboardForm/DashboardForm.js
+++ src/admin/compoents/dashboardForm/DashboardForm.js
@@ -1,60 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { makeStyles } from '@material-ui/core/styles';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
-import { Box, Typography, CircularProgress } from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import { Box, Typography, CircularProgress } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  box:{
+  box: {
     padding: theme.spacing(2),
-    marginLeft: '20px',
-    marginRight: '20px',
-    marginTop: '30px',
-    border: '0.5px solid #000',
-    backgroundColor: 'white',
+    marginLeft: "20px",
+    marginRight: "20px",
+    marginTop: "30px",
+    // border: "0.5px solid #000",
+    backgroundColor: "white",
     borderRadius: 5,
-    width: '1100px',
-    height: '400px',
-  boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.3)', // 그림자 효과
+    width: "1100px",
+    height: "400px",
+    boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.3)", // 그림자 효과
   },
   tooltip: {
-    backgroundColor: '#FFFFFF',
-    border: '0.5px solid #000000',
-    color: '#000000',
+    backgroundColor: "#FFFFFF",
+    border: "0.5px solid #000000",
+    color: "#000000",
     borderRadius: 10,
-    padding: '3px',
-    fontSize: '13px'
+    padding: "3px",
+    fontSize: "13px",
   },
   paper: {
     padding: theme.spacing(3),
     width: 850,
-    display: 'flex',
-    justifyContent: 'left',
-    alignItems: 'left',
-    marginLeft: '20px',
-    textAlign: 'left',
-    color: '#FFFFFF',
-    backgroundColor: '#3E3E3F',
+    display: "flex",
+    justifyContent: "left",
+    alignItems: "left",
+    marginLeft: "20px",
+    textAlign: "left",
+    color: "#FFFFFF",
+    backgroundColor: "#3E3E3F",
   },
   text01: {
-    borderBottom: '3px soild #3E3E3F',
-    marginTop: '10px',
-    marginLeft: '20px',
-    marginBottom: '30px',
-    fontSize: '28px',
-    fontWeight: 300,
-    color: '#000000',
-    textDecoration: 'underline',
-    textDecorationColor: '#000000',
-    textUnderlineOffset: '5px',
-    textDecorationThickness: '1px',
+    borderBottom: "3px soild #3E3E3F",
+    marginTop: "10px",
+    marginLeft: "20px",
+    marginBottom: "30px",
+    fontSize: "28px",
+    fontWeight: 700,
+    color: "#000000",
+    // textDecoration: "underline",
+    textDecorationColor: "#000000",
+    textUnderlineOffset: "5px",
+    textDecorationThickness: "1px",
   },
-
 }));
-
 
 function DashboardForm() {
   const classes = useStyles();
@@ -62,25 +68,30 @@ function DashboardForm() {
   //const [visitorCount, setVisitorCount] = useState([]); // 일일 방문자 수
   const recentWeek = getRecentWeek(); // 최근 일주일
   const [chartData, setChartData] = useState([]); // 차트 데이터
-  const [chartData02, setChartData02] = useState([]); 
+  const [chartData02, setChartData02] = useState([]);
   const [loading, setLoading] = useState(true);
   //const [markCount, setMarkCount] = useState([]); //상표신청수 데이터
 
   function getRecentWeek() {
     const today = new Date();
     const recentWeek = [];
-  
+
     for (let i = 0; i < 7; i++) {
-      const currentDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - i);
-      const dateString = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
+      const currentDate = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() - i
+      );
+      const dateString = `${currentDate.getFullYear()}-${
+        currentDate.getMonth() + 1
+      }-${currentDate.getDate()}`;
       recentWeek.push(dateString);
     }
-  
+
     return recentWeek.reverse();
   }
 
   function CustomTooltip({ active, payload, label }) {
-  
     if (active && payload && payload.length) {
       return (
         <div className={classes.tooltip}>
@@ -91,9 +102,8 @@ function DashboardForm() {
     }
     return null;
   }
-  
+
   function CustomTooltip2({ active, payload, label }) {
-  
     if (active && payload && payload.length) {
       return (
         <div className={classes.tooltip}>
@@ -106,46 +116,49 @@ function DashboardForm() {
   }
 
   useEffect(() => {
-    axios.all([
-      axios.get('http://localhost:8080/api/dashboard'),
-      axios.get('http://localhost:8080/api/main/user')
-    ]).then(axios.spread((dashboardResponse, infoResponse) => {
-      const dashboardData = dashboardResponse.data;
-      const infoData = infoResponse.data; //상표신청자데이터
-      const dateCounts = {}; // 날짜별 신청수
+    axios
+      .all([
+        axios.get("http://localhost:8080/api/dashboard"),
+        axios.get("http://localhost:8080/api/main/user"),
+      ])
+      .then(
+        axios.spread((dashboardResponse, infoResponse) => {
+          const dashboardData = dashboardResponse.data;
+          const infoData = infoResponse.data; //상표신청자데이터
+          const dateCounts = {}; // 날짜별 신청수
 
-      infoData.forEach((user) => {
+          infoData.forEach((user) => {
+            const date = user.created_at.substring(0, 10); // 날짜 형식 추출 (YYYY-MM-DD)
+            if (dateCounts[date]) {
+              dateCounts[date]++;
+            } else {
+              dateCounts[date] = 1;
+            }
+          });
 
-         const date = user.created_at.substring(0, 10); // 날짜 형식 추출 (YYYY-MM-DD)
-        if (dateCounts[date]) {
-          dateCounts[date]++;
-        } else {
-          dateCounts[date] = 1;
-        }
+          const chartData = Object.keys(dateCounts).map((date) => ({
+            name: date,
+            count: dateCounts[date] || 0,
+          }));
+
+          const updatedChartData = recentWeek.map((date) => ({
+            name: date,
+            visitor: dashboardData[recentWeek.indexOf(date)] || 0,
+            count: dateCounts[date] || 0,
+          }));
+
+          //setVisitorCount(dashboardData);
+          setChartData(updatedChartData);
+          setChartData02(chartData);
+          setLoading(false); // 로딩 완료 후 상태 업데이트
+          console.log(updatedChartData);
+        })
+      )
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
       });
-
-      const chartData = Object.keys(dateCounts).map((date) => ({
-        name: date,
-        count: dateCounts[date] || 0,
-      }));
-
-      const updatedChartData = recentWeek.map((date) => ({
-        name: date,
-        visitor: dashboardData[recentWeek.indexOf(date)] || 0,
-        count: dateCounts[date] || 0,
-      }));
-
-      //setVisitorCount(dashboardData);
-      setChartData(updatedChartData);
-      setChartData02(chartData);
-      setLoading(false); // 로딩 완료 후 상태 업데이트
-      console.log(updatedChartData);
-    })).catch((error) => {
-      console.log(error);
-      setLoading(false);
-    });
   }, []);
-  
 
   const integerFormatter = (value) => Math.floor(value);
   const [minValue, setMinValue] = useState(0);
@@ -164,61 +177,80 @@ function DashboardForm() {
   return (
     <div className={classes.root}>
       <Box className={classes.box}>
-      <Typography className={classes.text01}>
-        주간 방문자
-        </Typography>   
-      {loading ? (
-        <div style={{ 
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "300px",
-          width: "1000px" 
-          }}>
-          <CircularProgress color="inherit" size={80} />
-        </div>
-      ) : (
-        <AreaChart width={1000} height={300} data={chartData}>
-          <XAxis stroke='#000000' dataKey="name"/>
-          <YAxis stroke='#000000'
-          tickFormatter={integerFormatter}
-          domain={[minValue, maxValue]}
-          ticks={[minValue, (maxValue / 4), (maxValue / 2), (maxValue * 3 / 4), maxValue]}/>
-          <CartesianGrid stroke="#90827b" strokeDasharray="2 2" />
-          <Area dataKey="visitor" fill="#76777c" fillOpacity={0.8} stroke="#76777c" />
-          <Tooltip content={<CustomTooltip />} />
-          
-          <Legend />
-        </AreaChart>
-      )}
+        <Typography className={classes.text01}>주간 방문자</Typography>
+        {loading ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "300px",
+              width: "1000px",
+            }}
+          >
+            <CircularProgress color="inherit" size={80} />
+          </div>
+        ) : (
+          <AreaChart width={1000} height={300} data={chartData}>
+            <XAxis stroke="#000000" dataKey="name" />
+            <YAxis
+              stroke="#000000"
+              tickFormatter={integerFormatter}
+              domain={[minValue, maxValue]}
+              ticks={[
+                minValue,
+                maxValue / 4,
+                maxValue / 2,
+                (maxValue * 3) / 4,
+                maxValue,
+              ]}
+            />
+            <CartesianGrid stroke="#90827b" strokeDasharray="2 2" />
+            <Area
+              dataKey="visitor"
+              fill="#76777c"
+              fillOpacity={0.8}
+              stroke="#76777c"
+            />
+            <Tooltip content={<CustomTooltip />} />
+
+            <Legend />
+          </AreaChart>
+        )}
       </Box>
       <Box className={classes.box}>
-      <Typography className={classes.text01}>
-        상표신청 현황
-        </Typography> 
-      {loading ? (
-        <div style={{ 
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "300px",
-          width: "1000px" 
-          }}>
-          <CircularProgress color="inherit" size={80} />
-        </div>
-      ) : (
-        <AreaChart width={1000} height={300} data={chartData02}>
-          <XAxis stroke='#000000' dataKey="name"
-           tickCount={recentWeek.length} />
-          <YAxis stroke='#000000'
-          tickFormatter={integerFormatter}
-          />
-          <CartesianGrid stroke="#000000" strokeDasharray="2 2" />
-          <Area dataKey="count" fillOpacity={0.8} fill="#90827b" stroke="#90827b" />
-          <Tooltip content={<CustomTooltip2/>} />
-          <Legend />
-        </AreaChart>
-      )}
+        <Typography className={classes.text01}>상표신청 현황</Typography>
+        {loading ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "300px",
+              width: "1000px",
+            }}
+          >
+            <CircularProgress color="inherit" size={80} />
+          </div>
+        ) : (
+          <AreaChart width={1000} height={300} data={chartData02}>
+            <XAxis
+              stroke="#000000"
+              dataKey="name"
+              tickCount={recentWeek.length}
+            />
+            <YAxis stroke="#000000" tickFormatter={integerFormatter} />
+            <CartesianGrid stroke="#000000" strokeDasharray="2 2" />
+            <Area
+              dataKey="count"
+              fillOpacity={0.8}
+              fill="#90827b"
+              stroke="#90827b"
+            />
+            <Tooltip content={<CustomTooltip2 />} />
+            <Legend />
+          </AreaChart>
+        )}
       </Box>
     </div>
   );
