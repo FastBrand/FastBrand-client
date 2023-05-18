bed11ad51f5089d77b0acb721f92418e3045f6e6
ljw43 <leejiwon_tech@naver.com>
2023-05-18 14:37:01 +0900

    Update SideNavbarForm.js
    -상표목록 관리를 상표 관리로 수정

--- src/admin/compoents/sideNavbarForm/SideNavbarForm.js
+++ src/admin/compoents/sideNavbarForm/SideNavbarForm.js
@@ -1,10 +1,16 @@
 import { NavLink } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import { Drawer, List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
-import BookOutlinedIcon from '@mui/icons-material/BookOutlined';
-import SignalCellularAltOutlinedIcon from '@mui/icons-material/SignalCellularAltOutlined';
-import MessageOutlinedIcon from '@mui/icons-material/MessageOutlined';
-import logo from '../../../assets/images/logo/도형.svg';
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core";
+import BookOutlinedIcon from "@mui/icons-material/BookOutlined";
+import SignalCellularAltOutlinedIcon from "@mui/icons-material/SignalCellularAltOutlined";
+import MessageOutlinedIcon from "@mui/icons-material/MessageOutlined";
+import logo from "../../../assets/images/logo/도형.svg";
 
 const drawerWidth = 202;
 
@@ -15,13 +21,13 @@ const useStyles = makeStyles((theme) => ({
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    display: "flex", 
-    flexDirection: "column", 
+    display: "flex",
+    flexDirection: "column",
   },
   drawerPaper: {
     width: drawerWidth,
-    backgroundColor: '#3E3E3F',
-    color: '#FFFFFF'
+    backgroundColor: "#3E3E3F",
+    color: "#FFFFFF",
   },
   toolbar: {
     ...theme.mixins.toolbar,
@@ -31,14 +37,14 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(3),
   },
   icon: {
-    color: '#FFFFFF'
+    color: "#FFFFFF",
   },
   logo: {
-    width: '100px',
-    height: '100px',   
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: "auto", 
+    width: "100px",
+    height: "100px",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "auto",
     marginTop: "3px",
     marginBottom: "3px",
   },
@@ -49,7 +55,7 @@ function SideNavbarForm() {
 
   const handleListItemClick = (event) => {
     // 선택된 버튼의 스타일을 변경
-    event.currentTarget.style.backgroundColor = '#999999';
+    event.currentTarget.style.backgroundColor = "#999999";
   };
 
   return (
@@ -64,19 +70,34 @@ function SideNavbarForm() {
         <div className={classes.toolbar} />
         <img src={logo} alt="logo" className={classes.logo} />
         <List>
-          <ListItem button component={NavLink} to="/dashboard" onClick={handleListItemClick}>
+          <ListItem
+            button
+            component={NavLink}
+            to="/dashboard"
+            onClick={handleListItemClick}
+          >
             <ListItemIcon className={classes.icon}>
-              <SignalCellularAltOutlinedIcon/>
+              <SignalCellularAltOutlinedIcon />
             </ListItemIcon>
             <ListItemText primary="대시보드" />
           </ListItem>
-          <ListItem button component={NavLink} to="/markBoard" onClick={handleListItemClick}>
+          <ListItem
+            button
+            component={NavLink}
+            to="/markBoard"
+            onClick={handleListItemClick}
+          >
             <ListItemIcon className={classes.icon}>
-              <MessageOutlinedIcon/>
+              <MessageOutlinedIcon />
             </ListItemIcon>
-            <ListItemText primary="상품목록 관리" />
+            <ListItemText primary="상표 관리" />
           </ListItem>
-          <ListItem button component={NavLink} to="/faqBoard" onClick={handleListItemClick}>
+          <ListItem
+            button
+            component={NavLink}
+            to="/faqBoard"
+            onClick={handleListItemClick}
+          >
             <ListItemIcon className={classes.icon}>
               <BookOutlinedIcon />
             </ListItemIcon>
