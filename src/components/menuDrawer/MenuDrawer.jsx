f837c2a3fabbf50cccda44a15f870f32fdcdbffb
ljw43 <leejiwon_tech@naver.com>
2023-05-24 12:41:18 +0900

    Update MenuDrawer.jsx
    - menuItems 변경
    - 회사정보를 최상단으로 이동

--- src/components/menuDrawer/MenuDrawer.jsx
+++ src/components/menuDrawer/MenuDrawer.jsx
@@ -8,10 +8,10 @@ import { List, ListItemText, Grid, Box } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const menuItems = [
-  { text: "비용안내", link: "/price" },
+  { text: "서비스안내", link: "/companypoint" },
   { text: "절차안내", link: "/step" },
   { text: "회사안내", link: "/companyInfo" },
-  { text: "고객문의", link: "/faq" },
+  { text: "자주묻는질문", link: "/faq" },
   { text: "상표등록신청하기", link: "/domesticMark" },
 ];
 
@@ -22,6 +22,24 @@ const MenuDrawer = ({ open, toggleDrawer }) => {
       open={open}
       onClose={() => toggleDrawer(false)}
     >
+      <CustomBox>
+        <Grid container>
+          <Grid item xs={3}>
+            <Box style={{ fontWeight: 800 }}>
+              <Box>고객센터</Box>
+              <Box>핸드폰</Box>
+              <Box>이메일</Box>
+            </Box>
+          </Grid>
+          <Grid item xs={9}>
+            <Box>
+              <Box>02-553-1246</Box>
+              <Box>010-4006-5236</Box>
+              <Box>mark@wonjon.com</Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </CustomBox>
       <CustomCloseIcon onClick={() => toggleDrawer(false)} />
       <List>
         {menuItems.map((item, index) => (
@@ -30,24 +48,6 @@ const MenuDrawer = ({ open, toggleDrawer }) => {
           </CustomListItem>
         ))}
       </List>
-      <CustomBox>
-        <Grid container>
-          <Grid item xs={2}>
-            <Box style={{ fontWeight: 800 }}>
-              <Box>고객센터</Box>
-              <Box>핸드폰</Box>
-              <Box>이메일</Box>
-            </Box>
-          </Grid>
-          <Grid item xs={10}>
-            <Box>
-              <Box>02-553-1246 (무료상표출원상담)</Box>
-              <Box>010-4006-5236</Box>
-              <Box>mark@wonjon.com</Box>
-            </Box>
-          </Grid>
-        </Grid>
-      </CustomBox>
     </CustomDrawer>
   );
 };
