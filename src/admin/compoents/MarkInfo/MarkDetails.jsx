003aea808588a39c6329fc75d535864237f50318
kyw <96057775+blueberrycoffee@users.noreply.github.com>
2023-06-01 19:14:51 +0900

    seal 문제 해결

--- src/admin/compoents/MarkInfo/MarkDetails.jsx
+++ src/admin/compoents/MarkInfo/MarkDetails.jsx
@@ -6,7 +6,6 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import DeleteIcon from "@mui/icons-material/Delete";
 import moment from 'moment';
-import nonImage from '../../../assets/icon/iconCheck.svg';
 import "./paginate.css"
 
 function MarkDetail() {
@@ -56,7 +55,7 @@ function MarkDetail() {
             id: data.mark.id,
             brand_name: data.mark.brand_name,
             description: data.mark.description,
-            image: nonImage,
+            image: data.images[0]?.url,
             sector: data.mark.sector,
             type: data.mark.type,
             poc: data.mark.poc,
@@ -82,7 +81,7 @@ function MarkDetail() {
             corporateMobile: data.corporate.corporateMobile,
             corporatePhone: data.corporate.corporatePhone,
             corporateEmail: data.corporate.corporateEmail,
-            seal: data.seals[0].url || null,
+            seal: data.seals[0]?.url,
             caddress: data.corporate.address,
             cdetail: data.corporate.detail,
             czipcode: data.corporate.zipcode,
