22ce6e6fe8a95572094f7341fe3f459d4c0702d7
ljw43 <leejiwon_tech@naver.com>
2023-07-31 14:37:38 +0900

    Update Main.jsx
    - BackgroundBox 삭제
    - MainImg 추가
    - "상표등록" 문구 #0992E3으로 색 변경

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,19 +1,13 @@
-import {
-  CustomBox,
-  Title,
-  BackgroundBox,
-  ButtonLink,
-  SubTitle,
-} from "./MainStyle";
-import { Fade } from "@mui/material";
-import Footer from "../../components/footer/Footer";
 import React from "react";
 import { useEffect } from "react";
-import Navbar from "../../components/navbar/Navbar";
 import ReactGA from "react-ga";
+import { Fade } from "@mui/material";
+import { CustomBox, Title, ButtonLink, SubTitle, MainImg } from "./MainStyle";
+import Footer from "../../components/footer/Footer";
+import Navbar from "../../components/navbar/Navbar";
+import mainImg from "../../assets/images/mainPage.svg";
 
 const Main = () => {
-
   useEffect(() => {
     // Google Analytics 초기화
     ReactGA.initialize("UA-264549276-1");
@@ -24,27 +18,26 @@ const Main = () => {
 
   return (
     <>
-      <BackgroundBox>
-        <Navbar backgroundColor="none" />
+      <Navbar backgroundColor="none" />
 
-        <CustomBox>
-          <div>
-            <Fade timeout={1000} in={true} mountOnEnter unmountOnExit>
-              <Title variant="h1">
-                신속하고 합리적인
-                <br />
-                <span style={{ color: "#CBA585" }}>상표등록</span>
-              </Title>
-            </Fade>
-            <Fade timeout={1000} in={true} mountOnEnter unmountOnExit>
-              <SubTitle>고민하지 말고 신청하세요!</SubTitle>
-            </Fade>
-            <Fade timeout={2000} in={true} mountOnEnter unmountOnExit>
-              <ButtonLink to="/domesticMark">상표등록 신청하기</ButtonLink>
-            </Fade>
-          </div>
-        </CustomBox>
-      </BackgroundBox>
+      <CustomBox>
+        <div>
+          <Fade timeout={1000} in={true} mountOnEnter unmountOnExit>
+            <Title variant="h1">
+              신속하고 합리적인
+              <br />
+              <span style={{ color: "#0992E3" }}>상표등록</span>
+            </Title>
+          </Fade>
+          <Fade timeout={1000} in={true} mountOnEnter unmountOnExit>
+            <SubTitle>고민하지 말고 신청하세요!</SubTitle>
+          </Fade>
+          <Fade timeout={2000} in={true} mountOnEnter unmountOnExit>
+            <ButtonLink to="/domesticMark">상표등록 신청하기</ButtonLink>
+          </Fade>
+        </div>
+        <MainImg src={mainImg} />
+      </CustomBox>
       <Footer />
     </>
   );
